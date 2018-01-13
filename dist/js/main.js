767170c251ad983087a0b41dde4d016c7036f643
Olexiy-repin <lyoharepin@gmail.com>
2018-01-13 18:37:09 +0200

    add adaptiv for 768px and for 1920px

--- dist/js/main.js
+++ dist/js/main.js
@@ -15,6 +15,7 @@ $( ".cross" ).hide();
 $( ".hamburger" ).show();
 });
 });
+});
 
 // slider
 var slideIndex = 1;
@@ -27,12 +28,35 @@ function plusDivs(n) {
 function showDivs(n) {
   var i;
   var x = document.getElementsByClassName("mySlides");
+  var sp = document.getElementById("cnt");
+
   if (n > x.length) {slideIndex = 1}
   if (n < 1) {slideIndex = x.length}
   for (i = 0; i < x.length; i++) {
      x[i].style.display = "none";
   }
   x[slideIndex-1].style.display = "block";
+  sp.innerHTML=slideIndex;
 }
 
-});
+// bottom slider
+var counter = 1;
+show(counter);
+
+function plus(n) {
+  show(counter += n);
+}
+
+function show(n) {
+  var i;
+  var x = document.getElementsByClassName("slides");
+  var sp = document.getElementById("ct");
+
+  if (n > x.length) {counter = 1}
+  if (n < 1) {counter = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+  }
+  x[counter-1].style.display = "block";
+  sp.innerHTML=counter;
+}
